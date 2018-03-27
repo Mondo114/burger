78b7520d70c547cb1c04f21e6a02a82a3c022b5f
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-03-27 14:44:30 -0700

    next version

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fatstack", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Bypass", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Coronary", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Death by Burger Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Thick Slab", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("RIP Burger", false);
